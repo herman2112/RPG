596c04f6c51784f2a634e2e1b0f2e7ed7d0ed033
Daniel de Brito <danieldebrito@Daniels-MacBook-Air.local>
2023-05-14 11:29:26 -0300

    commit

--- game.py
+++ game.py
@@ -1,60 +1,67 @@
 import Character
 import pygame
+import tkinter as tk
 import sys
 
+pygame.init()
+clock = pygame.time.Clock()
+
+#tk_obj = tk.Tk()
+
+'''screen_info = {
+    'width': tk_obj.winfo_screenwidth(),
+    'height': tk_obj.winfo_screenheight()
+}'''
+
+screen = pygame.display.set_mode((1280, 720))
+
+pygame.display.set_caption('Jogo bom :)')
+
+warrior = pygame.image.load('assets/circle-41674.png')
+warrior = pygame.transform.scale(warrior, (200, 200))
+base_rectangle = warrior.get_rect()
+
+vY = 5
+vX = 5
+
 class Game:
-    def __init__(self, screen_width, screen_height, status = False):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.status = status
-    
-    # Class methods
-    @classmethod
-    def render_screen(cls, surf, color):
-        pass
-
-    # Instance methods
-    def run(self):
-        pygame.init()
-        
-        print("Game is running!")
-
-        self.status = True
-        
-        screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        
-        clock = pygame.time.Clock()
-        
-        pygame.display.set_caption('RPG nosso de cada dia')
-        
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    quit()
-            
-            screen.fill("white")
-
-            # Game render here
-
-            Game.render_screen()
-
-            pygame.display.flip()
-
-    def quit(self):
-        self.status = False
-        print("The game has quit!")
-        pygame.quit()
-        sys.quit()
-        return
-
+   def __init__(self, surface):
+       self._surface = surface
 
 def main():
+    game = Game(screen)
 
-    #root = tk.Tk()
-    
-    game = Game(1280, 720)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
 
-    game.run()
+        #LOGIC
+        key = pygame.key.get_pressed()
+
+        if key[pygame.K_w] and base_rectangle.top > 0:
+            base_rectangle.move_ip(0, -vY)
+        
+        if key[pygame.K_s] and 1280 - base_rectangle.top > 760:
+            
+            base_rectangle.move_ip(0, vY)
+
+        if key[pygame.K_a] and base_rectangle.left > 0:
+            base_rectangle.move_ip(-vX, 0)
+        
+        if key[pygame.K_d] and base_rectangle.left < 1080:
+            print(f"w: {screen.get_width()}")
+            print(f"dist: {base_rectangle.left}")
+            base_rectangle.move_ip(vX, 0)
+
+        #DRAW
+        screen.fill((0, 0, 0))
+        screen.blit(warrior, base_rectangle)
+        pygame.display.update()
+        clock.tick(60)
+
+
 
 if __name__ == '__main__':
     main()
